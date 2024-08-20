ea71323dbd9806d81e52fe0c9fed4828a928c56e
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-08-19 23:44:08 -0700

    completed

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -1,5 +1,8 @@
 fn main() {
     // You can optionally experiment here.
+    let b: [i32; 4] = [2, 4, 5, 6];
+    let sliced_test = &b[1..2];
+    println!("{:?}", sliced_test);
 }
 
 #[cfg(test)]
@@ -10,7 +13,7 @@ mod tests {
 
         // TODO: Get a slice called `nice_slice` out of the array `a` so that the test passes.
         // let nice_slice = ???
-
+        let nice_slice = &a[1..4];
         assert_eq!([2, 3, 4], nice_slice);
     }
 }
--- exercises/04_primitive_types/primitive_types6.rs
+++ exercises/04_primitive_types/primitive_types6.rs
@@ -1,5 +1,8 @@
 fn main() {
     // You can optionally experiment here.
+    let numbers = (2,3,4,5);
+    let third = numbers.2;
+    println!("{}", third);
 }
 
 #[cfg(test)]
@@ -11,7 +14,7 @@ mod tests {
         // TODO: Use a tuple index to access the second element of `numbers`
         // and assign it to a variable called `second`.
         // let second = ???;
-
+        let second = numbers.1;
         assert_eq!(second, 2, "This is not the 2nd number in the tuple!");
     }
 }
